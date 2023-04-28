383990a8eed50d3545d05576f736b2527189571d
1nfer <37373380+1nfer@users.noreply.github.com>
2023-04-28 00:33:42 -0500

    Update main.cpp

--- Dimensionalidad/main.cpp
+++ Dimensionalidad/main.cpp
@@ -4,6 +4,7 @@
 
 vector<Punto> GenPoints(int n, int k){ //Generar N puntos con k dimensiones
   vector<Punto> puntos;
+
   for(int i=0; i<n; i++){
     Punto A(k);
     puntos.push_back(A);
@@ -14,6 +15,7 @@ vector<Punto> GenPoints(int n, int k){ //Generar N puntos con k dimensiones
 vector<double> GetDistances(int n, int k){ //Calcular distancias entre todos los puntos
   vector<double> distancias;
   vector<Punto> puntos=GenPoints(n,k);
+
   for(int i=0; i<n; i++){
     for(int j=i+1; j<n; j++){
       Distancia D(puntos[i],puntos[j]);
@@ -26,7 +28,24 @@ vector<double> GetDistances(int n, int k){ //Calcular distancias entre todos los
 
 void Granularidad(int n, int k){
   vector<double> distancias=GetDistances(n,k);
-  
+  int nDiv=25; // #divisiones o barras
+  double sizeDiv = distancias.back()/nDiv; // tam division
+  int s = distancias.size();
+  double div = sizeDiv; //limite de la division
+  int count=0;
+  cout<<"Grado de Granularidad: "<<sizeDiv<<endl;
+  for(int i=0; i<nDiv; i++){
+    int amount = 0;
+      double d=distancias[count];
+    while(count<s && distancias[count] <= div){
+      double d=distancias[count];
+      amount++;
+      count++;
+    }
+    cout<<div<<endl;
+    cout<<amount<<endl;
+    div=div+sizeDiv;
+  }
 }
 
 int main(){
